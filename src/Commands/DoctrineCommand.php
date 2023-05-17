08cfcfc6fe099f7d99551e5652fc2d71284429b5
Ricardo Boss <contact@ricardoboss.de>
2023-05-17 11:29:53 +0200

    Pass raw command line to console runner

--- src/Commands/DoctrineCommand.php
+++ src/Commands/DoctrineCommand.php
@@ -9,7 +9,7 @@ use Elephox\Console\Command\CommandInvocation;
 use Elephox\Console\Command\CommandTemplateBuilder;
 use Elephox\Console\Command\Contract\CommandHandler;
 use Exception;
-use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 readonly class DoctrineCommand implements CommandHandler
@@ -35,10 +35,7 @@ readonly class DoctrineCommand implements CommandHandler
 		$doctrineCommand = $command->arguments->get('command')->string();
 
 		return ConsoleRunner::createApplication($this->entityManagerProvider)->run(
-			new ArrayInput([
-				'command' => $doctrineCommand,
-				...$command->raw->parameters->toList(),
-			]),
+			new StringInput($command->commandLine),
 			new ConsoleOutput(),
 		);
 	}
